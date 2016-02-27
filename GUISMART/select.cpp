7cf6e50ea8c839310a0c5334f5fe9c97b1352e5f
simone989 <simonedimauro989@gmail.com>
2016-02-27 01:50:40 +0100

    Aggiunta indirizzazione per la lettua del file per gli algo utilizzati

--- GUISMART/select.cpp
+++ GUISMART/select.cpp
@@ -2,6 +2,8 @@
 #include "ui_select.h"
 #include <QVBoxLayout>
 #include <QCheckBox>
+#include <fstream>
+#include <iostream>
 
  QCheckBox** ArrayCheck= new QCheckBox*[86];
 
@@ -17,6 +19,7 @@ Selezione::Selezione(QWidget *parent) :
 
 
 
+    system("./select -which >>file.txt");
 
     ArrayCheck[0]= new QCheckBox("bf",this);
     box2->addWidget(ArrayCheck[0]);
@@ -286,6 +289,8 @@ Selezione::Selezione(QWidget *parent) :
 
 
 
+
+
 }
 
 Selezione::~Selezione()
@@ -301,10 +306,93 @@ void Selezione::on_pushButton_clicked()
 void Selezione::on_pushButton_2_clicked()
 {
 
-    for(int i=0 ; i<86 ; i++){
-        if(ArrayCheck[i]->isChecked()){system("./select -which");  }
+    if(ArrayCheck[0]->isChecked()) system("./select bf");
+    if(ArrayCheck[1]->isChecked()) system("./select ");
+    if(ArrayCheck[2]->isChecked()) system("./select ");
+    if(ArrayCheck[3]->isChecked()) system("./select ");
+    if(ArrayCheck[4]->isChecked()) system("./select ");
+    if(ArrayCheck[5]->isChecked()) system("./select ");
+    if(ArrayCheck[6]->isChecked()) system("./select ");
+    if(ArrayCheck[7]->isChecked()) system("./select ");
+    if(ArrayCheck[8]->isChecked()) system("./select ");
+    if(ArrayCheck[9]->isChecked()) system("./select ");
+    if(ArrayCheck[10]->isChecked()) system("./select ");
+    if(ArrayCheck[11]->isChecked()) system("./select ");
+    if(ArrayCheck[12]->isChecked()) system("./select ");
+    if(ArrayCheck[13]->isChecked()) system("./select ");
+    if(ArrayCheck[14]->isChecked()) system("./select ");
+    if(ArrayCheck[15]->isChecked()) system("./select ");
+    if(ArrayCheck[16]->isChecked()) system("./select ");
+    if(ArrayCheck[17]->isChecked()) system("./select ");
+    if(ArrayCheck[18]->isChecked()) system("./select ");
+    if(ArrayCheck[19]->isChecked()) system("./select ");
+    if(ArrayCheck[20]->isChecked()) system("./select ");
+    if(ArrayCheck[21]->isChecked()) system("./select ");
+    if(ArrayCheck[22]->isChecked()) system("./select ");
+    if(ArrayCheck[23]->isChecked()) system("./select ");
+    if(ArrayCheck[24]->isChecked()) system("./select ");
+    if(ArrayCheck[25]->isChecked()) system("./select ");
+    if(ArrayCheck[26]->isChecked()) system("./select ");
+    if(ArrayCheck[27]->isChecked()) system("./select ");
+    if(ArrayCheck[28]->isChecked()) system("./select ");
+    if(ArrayCheck[29]->isChecked()) system("./select ");
+    if(ArrayCheck[30]->isChecked()) system("./select ");
+    if(ArrayCheck[31]->isChecked()) system("./select ");
+    if(ArrayCheck[32]->isChecked()) system("./select ");
+    if(ArrayCheck[33]->isChecked()) system("./select ");
+    if(ArrayCheck[34]->isChecked()) system("./select ");
+    if(ArrayCheck[35]->isChecked()) system("./select ");
+    if(ArrayCheck[36]->isChecked()) system("./select ");
+    if(ArrayCheck[37]->isChecked()) system("./select ");
+    if(ArrayCheck[38]->isChecked()) system("./select ");
+    if(ArrayCheck[39]->isChecked()) system("./select ");
+    if(ArrayCheck[40]->isChecked()) system("./select ");
+    if(ArrayCheck[41]->isChecked()) system("./select ");
+    if(ArrayCheck[42]->isChecked()) system("./select ");
+    if(ArrayCheck[43]->isChecked()) system("./select ");
+    if(ArrayCheck[44]->isChecked()) system("./select ");
+    if(ArrayCheck[45]->isChecked()) system("./select ");
+    if(ArrayCheck[46]->isChecked()) system("./select ");
+    if(ArrayCheck[47]->isChecked()) system("./select ");
+    if(ArrayCheck[48]->isChecked()) system("./select ");
+    if(ArrayCheck[49]->isChecked()) system("./select ");
+    if(ArrayCheck[50]->isChecked()) system("./select ");
+    if(ArrayCheck[51]->isChecked()) system("./select ");
+    if(ArrayCheck[52]->isChecked()) system("./select ");
+    if(ArrayCheck[53]->isChecked()) system("./select ");
+    if(ArrayCheck[54]->isChecked()) system("./select ");
+    if(ArrayCheck[55]->isChecked()) system("./select ");
+    if(ArrayCheck[56]->isChecked()) system("./select ");
+    if(ArrayCheck[57]->isChecked()) system("./select ");
+    if(ArrayCheck[58]->isChecked()) system("./select ");
+    if(ArrayCheck[59]->isChecked()) system("./select ");
+    if(ArrayCheck[60]->isChecked()) system("./select ");
+    if(ArrayCheck[61]->isChecked()) system("./select ");
+    if(ArrayCheck[62]->isChecked()) system("./select ");
+    if(ArrayCheck[63]->isChecked()) system("./select ");
+    if(ArrayCheck[64]->isChecked()) system("./select ");
+    if(ArrayCheck[65]->isChecked()) system("./select ");
+    if(ArrayCheck[66]->isChecked()) system("./select ");
+    if(ArrayCheck[67]->isChecked()) system("./select ");
+    if(ArrayCheck[68]->isChecked()) system("./select ");
+    if(ArrayCheck[69]->isChecked()) system("./select ");
+    if(ArrayCheck[70]->isChecked()) system("./select ");
+    if(ArrayCheck[71]->isChecked()) system("./select ");
+    if(ArrayCheck[72]->isChecked()) system("./select ");
+    if(ArrayCheck[73]->isChecked()) system("./select ");
+    if(ArrayCheck[74]->isChecked()) system("./select ");
+    if(ArrayCheck[75]->isChecked()) system("./select ");
+    if(ArrayCheck[76]->isChecked()) system("./select ");
+    if(ArrayCheck[77]->isChecked()) system("./select ");
+    if(ArrayCheck[78]->isChecked()) system("./select ");
+    if(ArrayCheck[79]->isChecked()) system("./select ");
+    if(ArrayCheck[80]->isChecked()) system("./select ");
+    if(ArrayCheck[81]->isChecked()) system("./select ");
+    if(ArrayCheck[82]->isChecked()) system("./select ");
+    if(ArrayCheck[83]->isChecked()) system("./select ");
+    if(ArrayCheck[84]->isChecked()) system("./select ");
+    if(ArrayCheck[85]->isChecked()) system("./select ");
 
-    }
 
 
     //  ArrayCheck[0]->add
